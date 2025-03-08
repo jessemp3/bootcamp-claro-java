db7064edb62c511b9e521100e9f7412223b71998
jesse <nitchsx@gmail.com>
2025-03-08 14:50:59 -0300

    feat: class of java

---
+++ Introducao/src/module1/First.java
@@ -0,0 +1,12 @@
+package module1;
+
+public class First {
+    public static void main(String[] args) {
+
+        String name = "jesse";
+        int age = 20;
+        String cat;
+
+        System.out.println("Hello, my name is " + name + " and I am " + age + " years old.");
+    }
+}
---
+++ Introducao/src/module2/TiposVariaveis.java
@@ -0,0 +1,14 @@
+package module2;
+
+public class TiposVariaveis {
+    public static void main(String[] args) {
+        float o = 1.0f; // forma de declarar um float , tem que obrigatorimente colocar o f no final
+        System.out.println(o);
+
+        int a = 12;
+        double b = (double) a; // casting
+
+       final int C = 10; // forma de declarar uma constante
+        C = 0;
+    }
+}
